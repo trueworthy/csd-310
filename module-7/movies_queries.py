f2d9a2c31c162c9ecbcae6b2775e06816a09d239
trueworthy <ltrueworthy@yahoo.com>
2024-12-01 07:40:00 -0700

    12/01/24 Module 8.2 Assignment: Movies: Update & Deletes

--- module-7/movies_queries.py
+++ module-7/movies_queries.py
@@ -3,9 +3,9 @@
 # CSD 310 - Module 7.2 Assignment: Movies Table Queries
 # Description: Write the code to connect to your MySQL movies database.
 # Write four queries, in one Python file including descriptions of output and format.
-    # The first and second query is to select all the fields for the studio and genre tables.
-    # The third query is to select the movie names for those movies that have a run time of less than two hours.
-    # The fourth query is to get a list of film names, and directors grouped by director.
+# The first and second query is to select all the fields for the studio and genre tables.
+# The third query is to select the movie names for those movies that have a run time of less than two hours.
+# The fourth query is to get a list of film names, and directors grouped by director.
 
 import mysql.connector
 from mysql.connector import errorcode
@@ -15,15 +15,52 @@ config = {
     "password": "popcorn",
     "host": "127.0.0.1",
     "database": "movies",
-    "raise_on_warnings": True
+    "raise_on_warnings": True,
 }
 
 try:
     db = mysql.connector.connect(**config)
-    
-    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
-    
-    input("\n\n Press any key to continue...")
+    cursor = db.cursor()
+
+    # Display the studio tables
+    cursor.execute("SELECT * FROM studio")
+    result = cursor.fetchall()
+
+    print("\n-- DISPLAYING Studio RECORDS --")
+
+    for row in result:
+        print("Studio id: {}\nStudio Name: {}\n".format(row[0], row[1]))
+
+    # Display the genre tables
+    cursor.execute("SELECT * FROM genre")
+    result = cursor.fetchall()
+
+    print("-- DISPLAYING Genre RECORDS --")
+
+    for row in result:
+        print("Genre id: {}\nGenre Name: {}\n".format(row[0], row[1]))
+
+    # Display films whose runtime is less than 120 minutes
+    cursor.execute("SELECT film_name,film_runtime FROM film where film_runtime<120")
+    result = cursor.fetchall()
+
+    print("-- DISPLAYING Film Runtime RECORDS --")
+
+    for row in result:
+        print("Film Name: {}\nRuntime: {}\n".format(row[0], row[1]))
+
+    # Display film names ordered by director
+    cursor.execute("SELECT film_name,film_director FROM film ORDER BY film_director")
+    result = cursor.fetchall()
+
+    print("-- DISPLAYING Director RECORDS in Order--")
+
+    for row in result:
+        print("Film Name: {}\nDirector: {}\n".format(row[0], row[1]))
+
+    # print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+    # input("\n\n Press any key to continue...")
 
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -36,4 +73,5 @@ except mysql.connector.Error as err:
         print(err)
 
 finally:
+    # close the cursor
     db.close()
--- module-8/movies_update_and_delete.py
+++ module-8/movies_update_and_delete.py
@@ -4,3 +4,73 @@
 # Description: Create a Python file named movies_update_and_delete.py. Connect to the movies database and use show_films(cursor, "DISPLAYING FILMS") to display the films.
     # Insert a new film (not 'Star Wars'), then update "Alien" to a Horror film and delete "Gladiator."
     # Use show_films after each change to display the updated list.
+
+import mysql.connector
+
+config = {
+    "user": "movies_user",
+    "password": "popcorn",
+    "host": "127.0.0.1",
+    "database": "movies",
+    "raise_on_warnings": True
+}
+
+# Establish the connection to the database
+db_connection = mysql.connector.connect(**config)
+
+# Create a cursor object to interact with the database
+cursor = db_connection.cursor()
+
+# The provided function to show films
+def show_films(cursor, title):
+    # Execute an inner join query to get the required data
+    cursor.execute('''SELECT film_name AS Name, film_director AS Director, genre_name AS Genre, studio_name AS 'Studio Name'
+                      FROM film 
+                      INNER JOIN genre ON film.genre_id = genre.genre_id 
+                      INNER JOIN studio ON film.studio_id = studio.studio_id''')
+    
+    # Fetch all the results from the executed query
+    films = cursor.fetchall()
+    
+    print("\n -- {} --".format(title))
+
+    # Display each film's details
+    for film in films:
+        print("Film Name: {}\nDirector: {}\nGenre Name: {}\nStudio Name: {}\n".format(film[0], film[1], film[2], film[3]))
+
+# Use the show_films function to display the films in the database
+show_films(cursor, "-- DISPLAYING FILMS --")
+
+# Insert a new film into the film table
+insert_query = "INSERT INTO film(film_name, film_releaseDate, film_runtime, film_director, studio_id, genre_id) " \
+                   "VALUES ('Serenity', '2005', '119', 'Joss Whedon', (SELECT studio_id FROM studio WHERE " \
+                  "studio_name = 'Universal Pictures'),(SELECT genre_id FROM genre WHERE genre_name = 'SciFi') ); "
+
+cursor.execute(insert_query)
+db_connection.commit()
+
+# Display films after inserting
+show_films(cursor, "-- DISPLAYING FILMS AFTER INSERT --")
+
+# Update the genre of "Alien" to Horror
+update_query = "update film set genre_id = (select genre_id from genre where genre_name = 'Horror') where " \
+                  "film_name = 'Alien'; "
+
+cursor.execute(update_query)
+db_connection.commit()
+
+# Display films after the update
+show_films(cursor, "DISPLAYING FILMS AFTER UPDATE - Changed Alien to Horror --")
+
+# Delete the movie "Gladiator"
+delete_query = " DELETE FROM film WHERE film_name = 'Gladiator';"
+
+cursor.execute(delete_query)
+db_connection.commit()
+
+# Display films after deleting
+show_films(cursor, "-- DISPLAYING FILMS AFTER DELETE --")
+
+# close the cursor and connection
+cursor.close()
+db_connection.close()
